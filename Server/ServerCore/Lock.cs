2804cbff97d88d073244a86e86c4bc2808b7ba50
OhJaeYoung <ostyoung@naver.com>
2023-02-19 23:47:47 +0900

    ReaderWriterLock Practice

---
+++ Server/ServerCore/Lock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    // 재귀적 락을 허용할지 (No)
+    // 스핀락 정책 (5000번 -> Yield)
+    class Lock
+    {
+        const int EMPTY_FLAG = 0x00000000;
+        const int WRITE_MASK = 0x7FFF0000;
+        const int READ_MASK = 0x0000FFFF;
+
+        // [Unused(1)] [WriteThreadId(15)] [ReadCount(16)]
+        int _flag;
+    }
+}
