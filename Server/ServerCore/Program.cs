371a3cdf0c770f0ee5366d7ad9153677f024bf50
OhJaeYoung <ostyoung@naver.com>
2023-02-20 19:43:55 +0900

    ReaderWriterLock Practice2

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -9,17 +9,18 @@ namespace ServerCore
 {
     class Program
     {
+        #region ReaderWriterLock
         // 1. 근성
         // 2. 양보
         // 3. 갑질
 
         // 상호배제
         // Monitor
-        static object _lock = new object();
-        static SpinLock _lock2 = new SpinLock();
+        static object _RWlock = new object();
+        static SpinLock _RWlock2 = new SpinLock();
 
         // RWLock ReaderWriterLock
-        static ReaderWriterLockSlim _lock3 = new ReaderWriterLockSlim();
+        static ReaderWriterLockSlim _RWlock3 = new ReaderWriterLockSlim();
 
 
         class Reward
@@ -30,9 +31,9 @@ namespace ServerCore
         // 99.999%
         static Reward GetRewardById(int id)
         {
-            _lock3.EnterReadLock();
+            _RWlock3.EnterReadLock();
 
-            _lock3.ExitReadLock();
+            _RWlock3.ExitReadLock();
             lock (_lock)
             {
 
@@ -43,21 +44,54 @@ namespace ServerCore
         // 0.001%
         static void AddReward(Reward reward)
         {
-            _lock3.EnterWriteLock();
+            _RWlock3.EnterWriteLock();
 
-            _lock3.ExitWriteLock();
+            _RWlock3.ExitWriteLock();
             lock (_lock)
             {
 
             }
         }
+        #endregion
 
+        #region ReaderWriterLock Practice
+        static volatile int count = 0;
+        static Lock _lock = new Lock();
+
+        static void ReaderWriterLockPractice()
+        {
+            Task t1 = new Task(delegate ()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    _lock.WriteLock();
+                    count++;
+                    _lock.WriteUnlock();
+                    _lock.WriteUnlock();
+                }
+            });
+            Task t2 = new Task(delegate ()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count--;
+                    _lock.WriteUnlock();
+                }
+            });
+
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine(count);
+        }
+        #endregion
         static void Main(string[] args)
         {
-            lock (_lock)
-            {
-
-            }            
+            ReaderWriterLockPractice();
         }
     }
 }
